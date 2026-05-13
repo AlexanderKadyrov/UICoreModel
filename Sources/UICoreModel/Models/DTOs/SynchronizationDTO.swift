34b0222cc18dfd86594cecc7dfb338e31fb32a93
Alexander <alexander-kad@yandex.ru>
2026-05-13 17:23:00 +0500

    fix sync (#7)

--- Sources/UICoreModel/Models/DTOs/SynchronizationDTO.swift
+++ Sources/UICoreModel/Models/DTOs/SynchronizationDTO.swift
@@ -2,12 +2,14 @@ import Foundation
 
 public struct SynchronizationDTO: Codable, Hashable {
     
+    public let templates: [TemplateDTO]
     public let elementTypes: [ElementTypeDTO]
     public let modifierTypes: [ModifierTypeDTO]
     public let elements: [ElementDTO]
     public let modifiers: [ModifierDTO]
     
-    public init(elementTypes: [ElementTypeDTO], modifierTypes: [ModifierTypeDTO], elements: [ElementDTO], modifiers: [ModifierDTO]) {
+    public init(templates: [TemplateDTO], elementTypes: [ElementTypeDTO], modifierTypes: [ModifierTypeDTO], elements: [ElementDTO], modifiers: [ModifierDTO]) {
+        self.templates = templates
         self.elementTypes = elementTypes
         self.modifierTypes = modifierTypes
         self.elements = elements
