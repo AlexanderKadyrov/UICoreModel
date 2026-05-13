00ccb169ac170d8ff189c4e988a305be946c804e
Alexander K <alexander-kad@yandex.ru>
2026-05-13 16:11:54 +0500

    fixes

--- Sources/UICoreModel/Models/DTOs/ModifierTypeDTO.swift
+++ Sources/UICoreModel/Models/DTOs/ModifierTypeDTO.swift
@@ -5,8 +5,15 @@ public struct ModifierTypeDTO: Codable, Hashable {
     public let id: Int?
     public let name: ModifierNameDTO
     
-    public init(id: Int?, name: ModifierNameDTO) {
+    public let createdAt: Date?
+    public let updatedAt: Date?
+    public let deletedAt: Date?
+    
+    public init(id: Int?, name: ModifierNameDTO, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
         self.name = name
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.deletedAt = deletedAt
     }
 }
