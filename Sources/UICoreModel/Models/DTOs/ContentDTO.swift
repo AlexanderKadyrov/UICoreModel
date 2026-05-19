739acc68a292a4d404c703b129b9b7a602495451
Alexander <alexander-kad@yandex.ru>
2026-05-20 01:28:09 +0500

    add content models (#9)

--- Sources/UICoreModel/Models/DTOs/ContentDTO.swift
+++ Sources/UICoreModel/Models/DTOs/ContentDTO.swift
@@ -1,18 +1,18 @@
 import Foundation
 
-public struct TemplateDTO: Codable, Hashable {
+public struct ContentDTO: Codable, Hashable {
     
     public let id: Int?
     
-    public let name: String
+    public let value: ContentValueDTO
     
     public let createdAt: Date?
     public let updatedAt: Date?
     public let deletedAt: Date?
     
-    public init(id: Int?, name: String, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
+    public init(id: Int?, value: ContentValueDTO, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
-        self.name = name
+        self.value = value
         self.createdAt = createdAt
         self.updatedAt = updatedAt
         self.deletedAt = deletedAt
