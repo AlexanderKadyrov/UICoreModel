3d846e78471c605856c6ae2ff48af3f1f0d0e49e
Alexander K <alexander-kad@yandex.ru>
2026-05-20 01:26:54 +0500

    fixes

--- Sources/UICoreModel/Models/DTOs/TemplateDTO.swift
+++ Sources/UICoreModel/Models/DTOs/TemplateDTO.swift
@@ -4,12 +4,15 @@ public struct TemplateDTO: Codable, Hashable {
     
     public let id: Int?
     
+    public let name: String
+    
     public let createdAt: Date?
     public let updatedAt: Date?
     public let deletedAt: Date?
     
-    public init(id: Int?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
+    public init(id: Int?, name: String, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
+        self.name = name
         self.createdAt = createdAt
         self.updatedAt = updatedAt
         self.deletedAt = deletedAt
