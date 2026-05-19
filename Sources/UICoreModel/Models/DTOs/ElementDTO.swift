abd15be054d20ab50fc59b9c5e08d178551230df
Alexander <alexander-kad@yandex.ru>
2026-05-20 01:53:02 +0500

    fix Synchronization model (#10)

--- Sources/UICoreModel/Models/DTOs/ElementDTO.swift
+++ Sources/UICoreModel/Models/DTOs/ElementDTO.swift
@@ -5,6 +5,7 @@ public struct ElementDTO: Codable, Hashable {
     public let id: Int?
     public let elementTypeId: Int?
     public let elementId: Int?
+    public let contentId: Int?
     public let templateId: Int?
     
     public let configuration: Configuration?
@@ -13,10 +14,11 @@ public struct ElementDTO: Codable, Hashable {
     public let updatedAt: Date?
     public let deletedAt: Date?
     
-    public init(id: Int?, elementTypeId: Int?, elementId: Int?, templateId: Int?, configuration: Configuration?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
+    public init(id: Int?, elementTypeId: Int?, elementId: Int?, contentId: Int?, templateId: Int?, configuration: Configuration?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
         self.elementTypeId = elementTypeId
         self.elementId = elementId
+        self.contentId = contentId
         self.templateId = templateId
         self.configuration = configuration
         self.createdAt = createdAt
--- Sources/UICoreModel/Models/DTOs/SynchronizationDTO.swift
+++ Sources/UICoreModel/Models/DTOs/SynchronizationDTO.swift
@@ -2,13 +2,15 @@ import Foundation
 
 public struct SynchronizationDTO: Codable, Hashable {
     
+    public let contents: [ContentDTO]
     public let templates: [TemplateDTO]
     public let elementTypes: [ElementTypeDTO]
     public let modifierTypes: [ModifierTypeDTO]
     public let elements: [ElementDTO]
     public let modifiers: [ModifierDTO]
     
-    public init(templates: [TemplateDTO], elementTypes: [ElementTypeDTO], modifierTypes: [ModifierTypeDTO], elements: [ElementDTO], modifiers: [ModifierDTO]) {
+    public init(contents: [ContentDTO], templates: [TemplateDTO], elementTypes: [ElementTypeDTO], modifierTypes: [ModifierTypeDTO], elements: [ElementDTO], modifiers: [ModifierDTO]) {
+        self.contents = contents
         self.templates = templates
         self.elementTypes = elementTypes
         self.modifierTypes = modifierTypes
