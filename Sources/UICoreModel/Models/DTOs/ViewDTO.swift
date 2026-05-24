120691dc614a9772fabfb7998c54f5f06adf29f1
Alexander K <alexander-kad@yandex.ru>
2026-05-24 16:17:02 +0500

    fixes

--- Sources/UICoreModel/Models/DTOs/ViewDTO.swift
+++ Sources/UICoreModel/Models/DTOs/ViewDTO.swift
@@ -4,16 +4,16 @@ public struct ViewDTO: Codable, Hashable {
     
     public let id: Int?
     public let parentId: Int?
-    public let templateId: Int?
+    public let componentId: Int?
     
     public let createdAt: Date?
     public let updatedAt: Date?
     public let deletedAt: Date?
     
-    public init(id: Int?, parentId: Int?, templateId: Int?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
+    public init(id: Int?, parentId: Int?, componentId: Int?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
         self.parentId = parentId
-        self.templateId = templateId
+        self.componentId = componentId
         self.createdAt = createdAt
         self.updatedAt = updatedAt
         self.deletedAt = deletedAt
