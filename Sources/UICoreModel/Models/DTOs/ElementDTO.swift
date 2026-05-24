d385dccef4ea7fcc0b46db278a1e43f5942cbb88
Alexander K <alexander-kad@yandex.ru>
2026-05-24 16:15:21 +0500

    fixes

--- Sources/UICoreModel/Models/DTOs/ElementDTO.swift
+++ Sources/UICoreModel/Models/DTOs/ElementDTO.swift
@@ -6,7 +6,7 @@ public struct ElementDTO: Codable, Hashable {
     public let elementTypeId: Int?
     public let parentId: Int?
     public let contentId: Int?
-    public let templateId: Int?
+    public let componentId: Int?
     
     public let configuration: Configuration?
     
@@ -14,12 +14,12 @@ public struct ElementDTO: Codable, Hashable {
     public let updatedAt: Date?
     public let deletedAt: Date?
     
-    public init(id: Int?, elementTypeId: Int?, parentId: Int?, contentId: Int?, templateId: Int?, configuration: Configuration?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
+    public init(id: Int?, elementTypeId: Int?, parentId: Int?, contentId: Int?, componentId: Int?, configuration: Configuration?, createdAt: Date?, updatedAt: Date?, deletedAt: Date?) {
         self.id = id
         self.elementTypeId = elementTypeId
         self.parentId = parentId
         self.contentId = contentId
-        self.templateId = templateId
+        self.componentId = componentId
         self.configuration = configuration
         self.createdAt = createdAt
         self.updatedAt = updatedAt
